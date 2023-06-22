64c199669837728f9e9839674dcda3b4c1f0a192
Adrian <adridevelop11@gmail.com>
2023-06-22 17:55:57 +0200

    Initial Commit

---
+++ PooPractica2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PooPractica2
+//
+//  Created by Adrian Velasco Carrasco on 22/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
